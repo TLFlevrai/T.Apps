db9c5516d60550788f42ff0b398a4f60f56fa70f
TLFlevrai <tlfralijaona@gmail.com>
2025-02-26 13:59:41 +0300

    [pre Alpha] 1.0.0.3
    rajout d'une case blanche a quoi sa sert ? (je sais pas)

--- T.Apps/T.OS.py
+++ T.Apps/T.OS.py
@@ -8,4 +8,7 @@ root.geometry("600x400")
 bottom_frame = tk.Frame(root, bg="lightgray", height=30)
 bottom_frame.pack(side=tk.BOTTOM, fill=tk.X)
 
+square = tk.Label(bottom_frame, bg="white", width=4, height=2, relief="solid", borderwidth=2)
+square.pack(pady=5, padx=10, side=tk.LEFT)
+
 root.mainloop()
