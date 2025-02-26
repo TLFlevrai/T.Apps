7f22c6d650bd5ba8de8e757eae91119d30f68d3a
TLFlevrai <tlfralijaona@gmail.com>
2025-02-26 14:27:29 +0300

    [pre Alpha] 1.0.0.5
    possibilite de changer le fond d'ecran et changement du nom de la fenetre

--- T.Apps/T.OS.py
+++ T.Apps/T.OS.py
@@ -2,11 +2,19 @@
 import tkinter as tk
 from files.Apps import create_bottom_bar
 
+def change_background(color):
+    if color == "black":
+        root.config(bg="black")
+        bottom_frame.config(bg="darkgray")
+    else:
+        root.config(bg="white")
+        bottom_frame.config(bg="lightgray")
+
 root = tk.Tk()
 
-root.title("Fenêtre Tkinter avec barre en bas")
+root.title("T.Apps")
 root.geometry("600x400")
 
-create_bottom_bar(root)
+bottom_frame = create_bottom_bar(root, change_background)
 
 root.mainloop()
--- T.Apps/files/Apps.py
+++ T.Apps/files/Apps.py
@@ -1,7 +1,8 @@
 # files/Apps.py
 import tkinter as tk
 
-def create_bottom_bar(parent):
+def create_bottom_bar(parent, change_background):
+
     bottom_frame = tk.Frame(parent, bg="lightgray", height=30)
     bottom_frame.pack(side=tk.BOTTOM, fill=tk.X)
 
@@ -10,5 +11,8 @@ def create_bottom_bar(parent):
 
     black_square = tk.Label(bottom_frame, bg="black", width=4, height=2, relief="solid", borderwidth=2)
     black_square.pack(pady=5, padx=10, side=tk.LEFT)
+
+    white_square.bind("<Button-1>", lambda event: change_background("white"))
+    black_square.bind("<Button-1>", lambda event: change_background("black"))
 
     return bottom_frame
