1a3ff9692758f338d83802c273f6ed845bc08f95
TLFlevrai <tlfralijaona@gmail.com>
2025-02-26 14:36:16 +0300

    [pre Alpha] 1.0.0.6
    nouveaux theme de fond ajouter

--- T.Apps/T.OS.py
+++ T.Apps/T.OS.py
@@ -1,4 +1,3 @@
-# T.OS.py
 import tkinter as tk
 from files.Apps import create_bottom_bar
 
@@ -6,13 +5,15 @@ def change_background(color):
     if color == "black":
         root.config(bg="black")
         bottom_frame.config(bg="darkgray")
+    elif color == "blue":
+        root.config(bg="#00A4D3")
+        bottom_frame.config(bg="#007BB5")
     else:
         root.config(bg="white")
         bottom_frame.config(bg="lightgray")
 
 root = tk.Tk()
-
-root.title("T.Apps")
+root.title("Fenêtre Tkinter avec barre en bas")
 root.geometry("600x400")
 
 bottom_frame = create_bottom_bar(root, change_background)
--- T.Apps/files/Apps.py
+++ T.Apps/files/Apps.py
@@ -1,8 +1,6 @@
-# files/Apps.py
 import tkinter as tk
 
 def create_bottom_bar(parent, change_background):
-
     bottom_frame = tk.Frame(parent, bg="lightgray", height=30)
     bottom_frame.pack(side=tk.BOTTOM, fill=tk.X)
 
@@ -12,7 +10,12 @@ def create_bottom_bar(parent, change_background):
     black_square = tk.Label(bottom_frame, bg="black", width=4, height=2, relief="solid", borderwidth=2)
     black_square.pack(pady=5, padx=10, side=tk.LEFT)
 
+    windows7_blue = "#00A4D3"
+    blue_square = tk.Label(bottom_frame, bg=windows7_blue, width=4, height=2, relief="solid", borderwidth=2)
+    blue_square.pack(pady=5, padx=10, side=tk.LEFT)
+
     white_square.bind("<Button-1>", lambda event: change_background("white"))
     black_square.bind("<Button-1>", lambda event: change_background("black"))
+    blue_square.bind("<Button-1>", lambda event: change_background("blue"))
 
     return bottom_frame
