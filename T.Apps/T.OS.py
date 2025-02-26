ced6d159432d601ac47aea66c93ec659df76f422
TLFlevrai <tlfralijaona@gmail.com>
2025-02-26 13:49:32 +0300

    [Pre.Alpha] 1.0.0.2
    fenetre  agrendie

--- T.Apps/T.OS.py
+++ T.Apps/T.OS.py
@@ -3,7 +3,7 @@ import tkinter as tk
 root = tk.Tk()
 
 root.title("Fenêtre Tkinter avec barre en bas")
-root.geometry("400x300")
+root.geometry("600x400")
 
 bottom_frame = tk.Frame(root, bg="lightgray", height=30)
 bottom_frame.pack(side=tk.BOTTOM, fill=tk.X)
